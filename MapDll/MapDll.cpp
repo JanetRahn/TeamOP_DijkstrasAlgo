058bdbe890dfccc141378864b459644018794abc
TimSchraml <timschraml@web.de>
2014-12-03 13:47:27 +0100

    Blatt 8 DLL Nachbarknoten

--- MapDll/MapDll.cpp
+++ MapDll/MapDll.cpp
@@ -4,30 +4,126 @@
 
 
 namespace DLLMapPath{
+
+	class Node {
+	public:
+		Node* preNode;
+		int distance;
+		bool flag;
+		int id;
+		Node *neightbours = new Node[4];
+
+		Node();
+
+		Node(int _dist, int _id, bool _flag){
+			distance = _dist;
+			id = _id;
+			flag = _flag;
+		}
+
+		void setDist(int dist){
+			distance = dist;
+		}
+
+		void setPreNode(Node pre){
+			*preNode = pre;
+		}
+
+		void setNB(Node* _neightbours){
+			neightbours = _neightbours;
+		}
+
+		void setFlag(bool flaged){
+			flag = flaged;
+		}
+
+		bool getFlag(){
+			return flag;
+		}
+
+		int getDistance(){
+			return distance;
+		}
+
+		int getID(){
+			return id;
+		}
+
+		Node* getNB(){
+			return neightbours;
+		}
+	};
+
 	extern "C" {
 		__declspec(dllexport) int* findPath(int from, int to, int* map, int mapw, int maph, int pathlength) {
 			printf("DLL berechnet weg von %d zu %d\n", from, to);
 			int* path = new int[pathlength];
-			//test
-			for (int i = 0; i < mapw*maph; i++){
+			int fieldWeight = mapw*maph;
+			Node* nodes = new Node[fieldWeight];
+			//Code Dij
+
+			for (size_t i = 0; i < fieldWeight; i++)
+			{
+				bool visit = false;
 				if (map[i]>0){
-					map[i] = 9999999;
+					visit = true;
 				}
-				else{
-					if (i == from || i == to){
-						map[i] = 0;
-					}
-					else{
-						map[i] = 1;
-					}
-				}
+				nodes[i] =*new Node(INFINITE,i,visit);
 			}
 
-			//endTest
+			for (size_t i = 0; i < fieldWeight; i++)
+			{
+				int n = i - mapw;
+				int o = i + 1;
+				int s = i + mapw;
+				int w = i - 1;
+				if (i < mapw){
+					n = 0;
+				}
+				if (i%mapw == 0){
+					w = 0;
+				}
+				if (i%mapw==(mapw-1)){
+					o = 0;
+				}
+				if (fieldWeight - mapw <= i){
+					s = 0;
+				}
+				Node neightbours[4] = { nodes[n], nodes[o], nodes[s], nodes[w] };
+				nodes[i].setNB(neightbours);
+			}
+			
+			nodes[from].setDist(1);
+
+			Node u = nodes[from];
+			while (allVisited(nodes, fieldWeight) == 0){
+				
+				Node* tmpNB = new Node[4];
+				tmpNB = u.getNB();
+
+				for (int i = 0; i < 4; i++){
+					
+						tmpNB[i].setPreNode(u);
+						tmpNB[i].setDist(
+							u.getDistance()
+							);
+					}
+				}			
+			}
+			//Code Dij
 			printf("Weg berechnet! ;) \n\n");
 			return path;
 		}
 
+		int allVisited(Node *nodes,int lengh){
+			int counter = 0;
+			for (int i = 0; i < lengh; i++){
+				if (!nodes[i].getFlag){
+					counter++;
+				}
+			}
+			return counter;
+		}
 		__declspec(dllexport) void freeArray(int* pointer) {
 			delete[] pointer;
 		}
